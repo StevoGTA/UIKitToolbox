d89bc4878f37b764ba56f546ddadd11c3e8ee2d6
Stevo <stevo@sunsetmagicwerks.com>
2020-04-08 18:31:24 -0700

    Added UIAlertController extension Added UIViewController extension Added UKTextField

---
+++ Source/Extensions/UIAlertController+Extensions.swift
@@ -0,0 +1,37 @@
+//
+//  UIAlertController+Extensions.swift
+//  Swift Toolbox
+//
+//  Created by Stevo on 3/20/20.
+//  Copyright © 2020 Stevo Brock. All rights reserved.
+//
+
+import UIKit
+
+//----------------------------------------------------------------------------------------------------------------------
+// MARK: UIAlertController extension
+extension UIAlertController {
+
+	// MARK: Lifecycle methods
+	//------------------------------------------------------------------------------------------------------------------
+	convenience init(title :String, message :String, preferredStyle :UIAlertController.Style = .alert,
+			actionButtonTitle :String, actionProc :@escaping () -> Void = {}) {
+		// Do super
+		self.init(title: title, message: message, preferredStyle: preferredStyle)
+
+		// Add action
+		addAction(UIAlertAction(title: actionButtonTitle, style: .default) { _ in actionProc() })
+	}
+
+	//------------------------------------------------------------------------------------------------------------------
+	convenience init(title :String, message :String, preferredStyle :UIAlertController.Style = .alert,
+			actionButtonTitle :String, cancelButtonTitle :String, actionProc :@escaping () -> Void,
+			cancelProc :@escaping () -> Void = {}) {
+		// Do super
+		self.init(title: title, message: message, preferredStyle: preferredStyle)
+
+		// Add actions
+		addAction(UIAlertAction(title: actionButtonTitle, style: .default) { _ in actionProc() })
+		addAction(UIAlertAction(title: cancelButtonTitle, style: .default) { _ in cancelProc() })
+	}
+}
