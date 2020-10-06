72af9cd5fc4542e0aa4a92562574cbe70ae651fe
Stevo <stevo@sunsetmagicwerks.com>
2020-10-06 11:11:29 -0700

    Added UKTSplitViewController.

---
+++ Source/Controller/UKTSplitViewController.swift
@@ -0,0 +1,33 @@
+//
+//  UKTSplitViewController.swift
+//  UIKit Toolbox
+//
+//  Created by Stevo on 10/6/20.
+//  Copyright © 2020 Stevo Brock. All rights reserved.
+//
+
+import UIKit
+
+//----------------------------------------------------------------------------------------------------------------------
+// MARK: UKTSplitViewController
+class UKTSplitViewController : UISplitViewController {
+
+	// MARK: Properties
+	private	var	notificationObservers = [NSObjectProtocol]()
+
+	// MARK: Lifecycle methods
+	//------------------------------------------------------------------------------------------------------------------
+	deinit {
+		// Cleanup
+		self.notificationObservers.forEach() { NotificationCenter.default.removeObserver($0) }
+	}
+
+	// MARK: Instance methods
+	//------------------------------------------------------------------------------------------------------------------
+	func addNotificationObserver(forName name :NSNotification.Name, object :Any? = nil, queue :OperationQueue? = nil,
+			proc :@escaping (Notification) -> Void) {
+		// Add
+		self.notificationObservers.append(
+				NotificationCenter.default.addObserver(forName: name, object: object, queue: queue, using: proc))
+	}
+}
